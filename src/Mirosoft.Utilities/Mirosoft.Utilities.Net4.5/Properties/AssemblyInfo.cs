df801442a23a448380abcd39102b9127a499185f
Amir Ismail <eng.amir.ismail@gmai.com>
2018-10-20 18:54:26 +0300

    support .net 4.5

--- src/Mirosoft.Utilities/Mirosoft.Utilities.Net4.5/Properties/AssemblyInfo.cs
+++ src/Mirosoft.Utilities/Mirosoft.Utilities.Net4.5/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mirosoft.Utilities.Net")]
+[assembly: AssemblyTitle("Mirosoft.Utilities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mirosoft.Utilities.Net")]
+[assembly: AssemblyProduct("Mirosoft.Utilities")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("883b9f8e-4fdd-416f-b266-3d76305290c1")]
+[assembly: Guid("e08f713b-65e8-490c-814e-2da42268611c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
